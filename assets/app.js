ca20e07f3ed5a1731f8b94b6f97eac2800d9c691
Alan <andrewsalan88@gmail.com>
2018-05-10 17:38:31 -0500

    working on still-animate functionality. added if script. not working yet

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ var countries = ["Venezuela", "Burundi", "Finland", "Laos"];
 
 function renderButtons() {
 
-    // Deleting the movies prior to adding new countries
+    // Deleting the countries prior to adding new countries
     $("#countryButtons").empty();
 
     // Looping through the array of countries
@@ -31,9 +31,10 @@ function renderButtons() {
  $("#add-country").on("click", function(event) {
     event.preventDefault();
     // console.log(countries.length);
-    var country = $("#country-input").val();
+    var country = $("#country-input").val().trim();
     countries.push(country);
     renderButtons();
+    
     // console.log(countries);
 
 
@@ -49,5 +50,83 @@ function renderButtons() {
   
 });
 
+    // Adding click event listen listener to all buttons
+    // $("#countryButtons").on("click", function() {
+        $(document).on("click", ".country-btn", displayGifs);
+    function displayGifs() {
+
+        
+        event.preventDefault();
+
+        // console.log(this);
+        
+
+
+        // Grabbing and storing the data-animal property value from the button
+        var countryName = $(this).attr("data-name");
+
+        $("#countries").empty();
+        
+        console.log(countryName);
+  
+        // Constructing a queryURL using the animal name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          countryName + "&api_key=dc6zaTOxFJmzC&limit=10";
+  
+        // Performing an AJAX request with the queryURL
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          // After data comes back from the request
+          .then(function(response) {
+            console.log(queryURL);
+  
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
+  
+            // Looping through each result item
+            for (var i = 0; i < results.length; i++) {
+  
+              // Creating and storing a div tag
+              var countryDiv = $("<div>");
+  
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + results[i].rating);
+  
+              // Creating and storing an image tag
+              var countryImage = $("<img>");
+              // Setting the src attribute of the image to a property pulled off the result item
+              countryImage.attr("src", results[i].images.fixed_height.url);
+  
+              // Appending the paragraph and image tag to the countryDiv
+              countryDiv.append(p);
+              countryDiv.append(countryImage);
+  
+              // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+              $("#countries").prepend(countryDiv);
+            }
+            
+          });
+
+        //   $(".country-btn").on("click", function() {
+        //     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        //     var state = $(this).attr("data-state");
+      
+            
+        //     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        //     // Then, set the image's data-state to animate
+        //     // Else set src to the data-still value
+        //     if (state === "still") {
+        //       $(this).attr("src", $(this).attr("data-animate"));
+        //       $(this).attr("data-state", "animate");
+        //     } else {
+        //       $(this).attr("src", $(this).attr("data-still"));
+        //       $(this).attr("data-state", "still");
+        //     }
+        //   });
+
+      };
 
 renderButtons()
