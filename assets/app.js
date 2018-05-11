184b262deca3887ad11f2c09782021304b014c85
Alan <andrewsalan88@gmail.com>
2018-05-10 21:52:58 -0500

    hell yeah

--- assets/app.js
+++ assets/app.js
@@ -117,15 +117,15 @@ function renderButtons() {
             
           });
 
-          $(".countryImage").on("click", function() {
+          $(document).on("click",".countryImage", function() {
             // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
+            // var state = $(countryImage).attr("data-state");
       
-            
+            console.log($(this).attr("data-state"));
             // If the clicked image's state is still, update its src attribute to what its data-animate value is.
             // Then, set the image's data-state to animate
             // Else set src to the data-still value
-            if (state === "still") {
+            if ($(this).attr("data-state") === "still") {
               $(this).attr("src", $(this).attr("data-animate"));
               $(this).attr("data-state", "animate");
             } else {
